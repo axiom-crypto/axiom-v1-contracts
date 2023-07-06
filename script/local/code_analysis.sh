7872df7e7174a6144c76490cbcbd53407b76fab8
John Whitton <john@johnwhitton.com>
2023-07-05 20:41:38 -0700

    Fix typing mistakes and improve coverage report

--- script/local/code_analysis.sh
+++ script/local/code_analysis.sh
@@ -1,11 +1,15 @@
 # generate log
-slither . --exclude-informational --exclude-low &> ./docs/axiom-v1-contracts-slither.log
+slither . --exclude-informational --exclude-low &> ./docs/axiom-v1-contracts.log
 
 # generate inheritance graph
 slither . --print inheritance-graph
-dot inheritance-graph.dot -Tpng -o axiom-v1-contracts-inheritance-graph.png
+dot inheritance-graph.dot -Tpng -o axiom-v1-contracts.png
 rm inheritance-graph.dot
-mv axiom-v1-contracts-inheritance-graph.png ./docs/.
+mv axiom-v1-contracts.png ./docs/.
 
 # run coverage report
-forge coverage &> ./docs/axiom-v1-contracts-coverage.log
+# forge coverage &> ./docs/axiom-v1-contracts-coverage.log
+forge coverage --report lcov
+lcov --remove lcov.info  -o lcov.info 'test/*' 'script/*'
+genhtml -o docs/coverage --branch-coverage lcov.info
+rm lcov.info
